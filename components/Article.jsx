325ae0cb8f4f58cef68a6c3fc7a1bad7abe8321f
bernard frimpong <nardfrimpong@gmail.com>
2021-08-10 09:11:58 +0000

    working with dates

--- components/Article.jsx
+++ components/Article.jsx
@@ -18,7 +18,7 @@ export default function Article({ post }) {
             <h5 className='text-center font-bold text-lg'>{post.title}</h5>
             <p className='text-center font-thin font-body text-sm m-2'>{post.description}</p>
             <p className='text-center mb-4'>
-              <small>Last updated {post.updated_at}</small>
+              <small>Last updated {post.updated_at.slice(-13,-2)}</small>
             </p>
             <div className="flex justify-center">
             <button className='  hover:text-white hover:bg-red-800 rounded-full py-2 px-3 uppercase text-xs font-bold curser-pointer tracking-wider  bg-transparent border-2 '>Read More</button>
